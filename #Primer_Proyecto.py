a8d39b03aafbbe351fa61c9ef02e3cf69f8c8667
Andres <Andrescardoza17@gmail.com>
2024-05-21 13:39:33 -0300

    modified: #Segundo_Proyecto.py Untracked files: Segundo_Proyecto.py

--- #Primer_Proyecto.py
+++ #Primer_Proyecto.py
@@ -1,15 +1,17 @@
 
 
 #Youtube_Prueba_busqueda
+#Se actualiza para probar github
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import time 
 driver=webdriver.Chrome()
 driver.get("https://www.youtube.com")
 driver.maximize_window()
 driver.find_element(By.NAME, "search_query").click()
 driver.find_element(By.NAME, "search_query").send_keys("MLB")
 driver.find_element(By.ID, "search-icon-legacy").click()
-import time 
+
 time.sleep(5)
 
 driver.quit()

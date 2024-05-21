75bf927d3f9ef6a8b0874f1e859ad8a86dbe16d8
Andres <Andrescardoza17@gmail.com>
2024-05-21 17:50:54 -0300

    modified. #Segundo_Proyecto.py

--- #Segundo_Proyecto.py
+++ #Segundo_Proyecto.py
@@ -9,12 +9,18 @@ driver=webdriver.Chrome()
 driver.get("https://demoqa.com/elements")
 driver.maximize_window()
 
-#Interactua con la pagina 
+#Interactua con un desplegable de una pagina 
 driver.find_element(By.CLASS_NAME, "icon").click()
 time.sleep(3)
 driver.find_element(By.CLASS_NAME, "icon").click()
 driver.find_element(By.ID, "item-0").click()
 
+#Aca podemos interactuar con datos en la caja de texto de la pagina
+driver.find_element(By.ID, "userName").send_keys("Andres")
+driver.find_element(By.ID, "userEmail").send_keys("Andres@prueba.com")
+driver.find_element(By.ID, "currentAddress").send_keys("Av Prueba 5500")
+driver.find_element(By.ID, "permanentAddress").send_keys("Av Prueba 5500"+Keys.TAB)
+driver.find_element(By.ID, "submit").click()
 #Tiempo de espera
 time.sleep(8)
 

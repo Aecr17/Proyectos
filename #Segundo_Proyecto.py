8a29111df2669887935826a820940958a7af8f21
Andres <Andrescardoza17@gmail.com>
2024-05-22 10:43:59 -0300

    modified. #Segundo_Proyecto.py modified: README.md

--- #Segundo_Proyecto.py
+++ #Segundo_Proyecto.py
@@ -8,20 +8,21 @@ import time
 driver=webdriver.Chrome()
 driver.get("https://demoqa.com/elements")
 driver.maximize_window()
-# se agrega un nuevo time sleep
-time.sleep(2)
+
 #Interactua con un desplegable de una pagina 
-driver.find_element(By.CLASS_NAME, "icon").click()
-time.sleep(3)
-driver.find_element(By.CLASS_NAME, "icon").click()
+dropdown =driver.find_element(By.CLASS_NAME, "icon").click()
+time.sleep(2)
+dropdown= driver.find_element(By.CLASS_NAME, "icon").click()
 driver.find_element(By.ID, "item-0").click()
 
 #Aca podemos interactuar con datos en la caja de texto de la pagina
-driver.find_element(By.ID, "userName").send_keys("Andres")
-driver.find_element(By.ID, "userEmail").send_keys("Andres@prueba.com")
-driver.find_element(By.ID, "currentAddress").send_keys("Av Prueba 5500")
-driver.find_element(By.ID, "permanentAddress").send_keys("Av Prueba 5500"+Keys.TAB)
-driver.find_element(By.ID, "submit").click()
+text_username = driver.find_element(By.ID, "userName").send_keys("Andres")
+text_email = driver.find_element(By.ID, "userEmail").send_keys("Andres@prueba.com")
+text_currentAddress = driver.find_element(By.ID, "currentAddress").send_keys("Av Prueba 5500")
+text_permanetAdderss =driver.find_element(By.ID, "permanentAddress").send_keys("Av Prueba 5500"+Keys.TAB)
+btn_send = driver.find_element(By.ID, "submit")
+btn_send.click()
+
 #Tiempo de espera
 time.sleep(8)
 
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Se actualizo el codigo del titulo #Segundo_Proyecto.py referido a la wed DEMOQA
 Se agregaron lineas de codigo donde se interactua con el boton "Submit"
 
 Se modifico la linea de codigo agregnado un nueov time sleep despues de maximizar la pantalla
+
+Se agregaron funciones a las interacciones con el desplegable, caja de texto y boton ''Submit''
